1d6572c0e661feff8ee5ff75d67eb6a2904294cb
fist22 <hnamanie1@gmail.com>
2020-08-12 22:18:41 +0530

    Adding products from cart to orders

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 
 const app = express();
@@ -60,6 +62,11 @@ Cart.belongsToMany(Product, {
 });
 Product.belongsToMany(Cart, {
   through: CartItem
+});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {
+  through: OrderItem
 });
 
 
